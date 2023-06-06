0ec8ace232c81589476daef26b5fdfb50d7b5a87
vluz <vluz@users.noreply.github.com>
2023-06-06 19:37:35 +0100

    Update localwhisper.py

--- localwhisper.py
+++ localwhisper.py
@@ -6,7 +6,10 @@ import tempfile
 
 def transcribe_audio(file, model):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".tmp") as temp_file:
-        temp_file.write(file.getvalue())
+        try:
+            temp_file.write(file.getvalue())
+        except AttributeError:
+            temp_file.write(file)
         temp_file.flush()
     model = whisper.load_model(model)
     transcript = model.transcribe(temp_file.name)
@@ -17,7 +20,10 @@ st.title("Whisper local speech-to-text")
 st.divider()
 option = st.selectbox('Please select a model for transcription', ('small', 'base', 'tiny'))
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3", "m4a", "ogg"])
-st.markdown("[![test.mp3](app/static/test.mp3)](https://streamlit.io)")
+if st.button("Use test file"):
+    f = open("test.mp3", "rb")
+    uploaded_file = f.read()
+    f.close()
 st.divider()
 if uploaded_file is not None:
     st.audio(uploaded_file)
